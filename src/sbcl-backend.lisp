94f7cf7b25fb823ed9f44ab8822f18c618c930be
Alexandr Popolitov <popolit@gmail.com>
2014-12-10 14:02:52 +0100

    Fix for SBCL 1.2.6

--- src/sbcl-backend.lisp
+++ src/sbcl-backend.lisp
@@ -7,23 +7,76 @@
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defun new-style-fun-type-p (lst)
-    (and (consp lst)
-	 (equal 4 (length lst))
-	 (destructuring-bind (val v1 v2 opt) lst
-	   (and (equal val 'values)
-		(eq v2 't)
-		(eq opt '&optional))))))
+    (equal '(values t t &optional) lst))
+  (defun very-new-style-fun-type-p (lst)
+    (equal '(values bit t &optional) lst)))
+  
 
 (eval-when (:compile-toplevel :execute)
   (let ((it (caddr (sb-impl::%fun-type #'sb-impl::read-maybe-nothing))))
-    (cond ((new-style-fun-type-p it)
-	   (pushnew 'new-style-readlist *features*))
-	  ((equal '* it) nil)
+    (cond ((new-style-fun-type-p it) (pushnew 'new-style-readlist *features*))
+	  ((very-new-style-fun-type-p it) (pushnew 'very-new-style-readlist *features*))
+	  ((equal '* it) (pushnew 'old-style-readlist *features*))
 	  (t (pushnew 'not-supported-sbcl *features*)))))
 
 (in-package "SB-IMPL")
 
 (defparameter *read-macro-tokens* (make-hash-table :test #'eq))
+
+#+cl-read-macro-tokens-patch::very-new-style-readlist
+(macrolet
+    ((with-list-reader ((streamvar delimiter) &body body)
+       `(let* ((thelist (list nil))
+               (listtail thelist)
+               (collectp (if *read-suppress* 0 -1))
+	       (first-time t))
+          (declare (dynamic-extent thelist))
+          (loop (let ((firstchar (flush-whitespace ,streamvar)))
+                  (when (eq firstchar ,delimiter)
+                    (return (cdr thelist)))
+                  ,@body))))
+     (read-list-item (streamvar)
+       `(multiple-value-bind (winp obj)
+            (read-maybe-nothing ,streamvar firstchar)
+          ;; allow for a character macro return to return nothing
+          (unless (zerop (logand winp collectp))
+	    (if first-time
+		(let ((custom-reader (gethash obj *read-macro-tokens*)))
+		  (if custom-reader
+		      (return-from read-list-new (funcall custom-reader stream obj)))))
+            (setq listtail
+                  (cdr (rplacd (truly-the cons listtail) (list obj))))
+	    (setq first-time nil)))))
+  ;;; The character macro handler for left paren
+  (defun read-list-new (stream ignore)
+    (declare (ignore ignore))
+    (with-list-reader (stream #\))
+      (when (eq firstchar #\.)
+        (let ((nextchar (read-char stream t)))
+          (cond ((token-delimiterp nextchar)
+                 (cond ((eq listtail thelist)
+                        (unless (zerop collectp)
+                          (simple-reader-error
+                           stream "Nothing appears before . in list.")))
+                       ((whitespace[2]p nextchar)
+                        (setq nextchar (flush-whitespace stream))))
+                 (rplacd (truly-the cons listtail)
+                         (read-after-dot stream nextchar collectp))
+                 ;; Check for improper ". ,@" or ". ,." now rather than
+                 ;; in the #\` reader. The resulting QUASIQUOTE macro might
+                 ;; never be exapanded, but nonetheless could be erroneous.
+                 (unless (zerop (logand *backquote-depth* collectp))
+                   (let ((lastcdr (cdr (last listtail))))
+                     (when (and (comma-p lastcdr) (comma-splicing-p lastcdr))
+                       (simple-reader-error
+                        stream "~S contains a splicing comma after a dot"
+                        (cdr thelist)))))
+                 (return (cdr thelist)))
+                    ;; Put back NEXTCHAR so that we can read it normally.
+                (t (unread-char nextchar stream)))))
+      ;; Next thing is not an isolated dot.
+      (read-list-item stream))))
+
 
 #+cl-read-macro-tokens-patch::new-style-readlist
 (defun read-list-new (stream ignore)
@@ -68,7 +121,7 @@
           (setq listtail (cdr (rplacd listtail (list obj))))
 	  (setq first-time nil))))))
 
-#-cl-read-macro-tokens-patch::new-style-readlist
+#+cl-read-macro-tokens-patch::old-style-readlist
 (defun read-list-new (stream ignore)
   (declare (ignore ignore))
   (let* ((thelist (list nil))
